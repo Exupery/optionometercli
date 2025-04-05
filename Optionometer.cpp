c3e04057b1d4333669414346c501c43a526a40a3
Matthew Frost <Exupery@users.noreply.github.com>
2025-04-05 18:07:45 -0400

    read ticker from args

--- Optionometer.cpp
+++ Optionometer.cpp
@@ -1,7 +1,9 @@
 ﻿#include "Optionometer.h"
 
+const auto DEFAULT_TICKER = "QQQ";
 
-int main() {
-	std::cout << "Optionometer" << std::endl;
-	return 0;
+int main(const int argc, const char* argv[]) {
+  const auto ticker = (argc > 1) ? argv[1] : DEFAULT_TICKER;
+  std::cout << "Optionometer started with ticker " << ticker << std::endl;
+  return 0;
 }
