66c2ac8b55da4115eb14ef5a43709d499ddad8a5
Matthew Frost <Exupery@users.noreply.github.com>
2025-04-21 20:57:33 -0400

    read min/max days properties in app context

--- src/main/kotlin/com/optionometer/main/AppConfiguration.kt
+++ src/main/kotlin/com/optionometer/main/AppConfiguration.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.optionometer.quotes.Importer
 import com.optionometer.quotes.marketdata.MarketDataImporter
+import com.optionometer.screener.Screener
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -25,6 +26,15 @@ class AppConfiguration {
     objectMapper: ObjectMapper
   ): Importer {
     return MarketDataImporter(rootEndpoint, objectMapper)
+  }
+
+  @Bean
+  fun screener(
+    importer: Importer,
+    @Value("\${screener.expiration.minDays}") minDays: Int,
+    @Value("\${screener.expiration.maxDays}") maxDays: Int
+  ): Screener {
+    return Screener(importer, minDays, maxDays)
   }
 
 }
--- src/main/kotlin/com/optionometer/screener/Screener.kt
+++ src/main/kotlin/com/optionometer/screener/Screener.kt
@@ -1,15 +1,15 @@
 package com.optionometer.screener
 
+import com.optionometer.models.OptionChain
 import com.optionometer.quotes.Importer
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class Screener(
   private val importer: Importer,
-  @Value("\${screener.expiration.minDays}") private val minDays: Int,
-  @Value("\${screener.expiration.maxDays}") private val maxDays: Int
+  private val minDays: Int,
+  private val maxDays: Int
 ) {
 
   private val logger = LoggerFactory.getLogger(javaClass)
@@ -17,6 +17,11 @@ class Screener(
   fun screen(ticker: String) {
     logger.info("Screening option trades for $ticker between $minDays and $maxDays days to expiration")
     val optionChains = importer.fetchOptionChains(ticker, minDays, maxDays)
+    val bestByExpiry = optionChains.map { findBestForChain(it) }
+  }
+
+  private fun findBestForChain(optionChain: OptionChain) {
+
   }
 
 }
