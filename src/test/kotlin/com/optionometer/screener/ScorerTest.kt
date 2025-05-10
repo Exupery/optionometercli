c3245b54868af9346b23b30b7fcde4ede1cfc933
Matthew Frost <Exupery@users.noreply.github.com>
2025-05-10 16:10:04 -0400

    add more scorer tests

--- src/test/kotlin/com/optionometer/screener/ScorerTest.kt
+++ src/test/kotlin/com/optionometer/screener/ScorerTest.kt
@@ -5,16 +5,39 @@ import com.optionometer.utils.call
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.reflect.KVisibility
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.isAccessible
-import kotlin.test.assertNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ScorerTest {
+
+  private val underlyingPrice = 10.0
+  private val call = mockk<Option>()
+  private val put = mockk<Option>()
+  private val smallProfit = mockk<Trade>()
+  private val largeProfit = mockk<Trade>()
+
+  @BeforeEach
+  fun setup() {
+    every { call.impliedVolatility } returns 0.5
+    every { call.dte } returns 365
+    every { put.impliedVolatility } returns 0.5
+    every { put.dte } returns 365
+    every { smallProfit.buys } returns listOf(call)
+    every { smallProfit.sells } returns emptyList()
+    every { smallProfit.profitLossAtPrice(any()) } returns -1.0
+    every { smallProfit.profitLossAtPrice(underlyingPrice) } returns 100.0
+    every { smallProfit.profitLossAtPrice(underlyingPrice + 1.0) } returns 100.0
+    every { largeProfit.buys } returns emptyList()
+    every { largeProfit.sells } returns listOf(call)
+    every { largeProfit.profitLossAtPrice(any()) } returns 200.0
+  }
 
   @Test
   fun `empty list is gracefully handled`() {
@@ -29,27 +52,41 @@ class ScorerTest {
     every { unprofitableTrade.buys } returns listOf(option)
     every { unprofitableTrade.profitLossAtPrice(any()) } returns -1.0
 
-    val actual = callPrivateScore(unprofitableTrade, 10.0)
+    val actual = callPrivateScore(unprofitableTrade)
     assertNull(actual)
   }
 
   @Test
   fun `scores pl by price points`() {
-    val option = mockk<Option>()
-    every { option.impliedVolatility } returns 0.5
-    every { option.dte } returns 365
-    val trade = mockk<Trade>()
-    every { trade.buys } returns listOf(option)
-    every { trade.sells } returns emptyList()
-    every { trade.profitLossAtPrice(any()) } returns 1.0
-    // TODO COMPARE TO MORE PROFITABLE TRADE
-
-    val actual = callPrivateScore(trade, 10.0)
-    assertNotNull(actual)
-    assertTrue(actual!!.score.pricePointScore > 0)
+    val smallProfitScore = callPrivateScore(smallProfit)
+    assertNotNull(smallProfitScore)
+    assertTrue(smallProfitScore!!.score.pricePointScore > 0)
+    val largeProfitScore = callPrivateScore(largeProfit)
+    assertNotNull(largeProfitScore)
+    assertTrue(largeProfitScore!!.score.pricePointScore > smallProfitScore.score.pricePointScore)
   }
 
-  private fun callPrivateScore(trade: Trade, underlyingPrice: Double): RawScoredTrade? {
+  @Test
+  fun `scores by number of profitable price points`() {
+    val smallProfitScore = callPrivateScore(smallProfit)
+    assertNotNull(smallProfitScore)
+    assertTrue(smallProfitScore!!.score.numProfitablePointsScore > 0)
+    val largeProfitScore = callPrivateScore(largeProfit)
+    assertNotNull(largeProfitScore)
+    assertTrue(largeProfitScore!!.score.numProfitablePointsScore > smallProfitScore.score.numProfitablePointsScore)
+  }
+
+  @Test
+  fun `scores by probability of profit`() {
+    val smallProfitScore = callPrivateScore(smallProfit)
+    assertNotNull(smallProfitScore)
+    assertTrue(smallProfitScore!!.score.scoreByProbability > 0)
+    val largeProfitScore = callPrivateScore(largeProfit)
+    assertNotNull(largeProfitScore)
+    assertTrue(largeProfitScore!!.score.scoreByProbability > smallProfitScore.score.scoreByProbability)
+  }
+
+  private fun callPrivateScore(trade: Trade): RawScoredTrade? {
     return Scorer::class.declaredFunctions
       .first { it.name == "score" && it.visibility == KVisibility.PRIVATE }
       .apply { isAccessible = true }
